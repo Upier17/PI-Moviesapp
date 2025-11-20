30e4d72298f5dbe6868a95d0843b3c0bb5ce7eff
alexanddark <alexander_17_@live.com>
2025-11-20 01:49:09 -0600

    database update4

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 export const db = await mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
+  host: process.env.MYSQLHOST,     // centerbeam.proxy.rlwy.net
+  user: process.env.MYSQLUSER,     // root
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  port: process.env.MYSQLPORT,     // 42524
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
